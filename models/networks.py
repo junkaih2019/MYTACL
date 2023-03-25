0c3d879c42985faafb32c0b69935f112160a5b00
Junkai <1054630406@qq.com>
2023-03-25 09:57:35 +0800

    添加Lcp损失函数

--- models/networks.py
+++ models/networks.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch.nn import init
 import functools
 from torch.optim import lr_scheduler
+from models.vgg import Vgg19
 
 
 ###############################################################################
@@ -614,3 +615,51 @@ class PixelDiscriminator(nn.Module):
     def forward(self, input):
         """Standard forward."""
         return self.net(input)
+
+class MeanShift(nn.Conv2d):
+    def __init__(self, data_mean, data_std, data_range=1, norm=True):
+        """norm (bool): normalize/denormalize the stats"""
+        c = len(data_mean)
+        super(MeanShift, self).__init__(c, c, kernel_size=1)
+        std = torch.Tensor(data_std)
+        self.weight.data = torch.eye(c).view(c, c, 1, 1)
+        if norm:
+            self.weight.data.div_(std.view(c, 1, 1, 1))
+            self.bias.data = -1 * data_range * torch.Tensor(data_mean)
+            self.bias.data.div_(std)
+        else:
+            self.weight.data.mul_(std.view(c, 1, 1, 1))
+            self.bias.data = data_range * torch.Tensor(data_mean)
+        self.requires_grad = False
+
+class VGGLoss1(nn.Module):
+    def __init__(self, device, vgg=None, weights=None, indices=None, normalize=True):
+        super(VGGLoss1, self).__init__()
+        if vgg is None:
+            self.vgg = Vgg19().cuda()
+        else:
+            self.vgg = vgg
+        self.criterion = nn.L1Loss()
+        # self.weights = weights or [1.0 / 2.6, 1.0 / 4.8]
+        # self.indices = indices or [2, 7]
+        self.weights = weights or [1.0 / 32., 1.0 / 16., 1.0 / 8., 1.0 / 4., 1.]
+        self.indices = indices or [1, 3, 5, 9, 13]
+        self.device = device
+        if normalize:
+            self.normalize = MeanShift([0.485, 0.456, 0.406], [0.229, 0.224, 0.225], norm=True).to(self.device)
+        else:
+            self.normalize = None
+        print("Vgg: Weights: ", self.weights, " indices: ", self.indices, " normalize: ", self.normalize)
+
+    def __call__(self, x, y, z):
+        if self.normalize is not None:
+            x = self.normalize(x)
+            y = self.normalize(y)
+            z = self.normalize(z)
+        x_vgg, y_vgg, z_vgg = self.vgg(x, self.indices), self.vgg(y, self.indices), self.vgg(z, self.indices)
+        loss = 0
+        for i in range(len(x_vgg)):
+            loss += self.weights[i] * (self.criterion(x_vgg[i], y_vgg[i].detach()) / self.criterion(z_vgg[i], y_vgg[i].detach()))
+
+        return loss
+
